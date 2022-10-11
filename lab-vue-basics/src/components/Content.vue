87eaf542547c992f098237d32d566485c1de272e
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-11 16:01:26 +0200

    iteration 4 done

--- lab-vue-basics/src/components/Content.vue
+++ lab-vue-basics/src/components/Content.vue
@@ -8,8 +8,16 @@
 
 <h1 class="is-size-1">Iteration 3</h1>
 <h2 v-if="showText">Este texto es visible si su variable es true</h2>
-</template>
 
+<h1 class="is-size-1">Iteration 4</h1>
+<ul>
+    <li v-for="post in posts"><b>Title</b>: {{post.title}} // <b>Description</b>: {{post.description}} // <b>Content</b>: {{post.content}}</li>
+</ul>
+
+</template>
+<!-- END TEMPLATE -->
+
+<!-- START SCRIPT -->
 <script setup>
 import { ref } from 'vue';
 
@@ -18,6 +26,24 @@ function showString (){
 }
 
 let showText = true;
+
+let posts = [
+    {
+        title: "Unseen Tribunal",
+        description: "Attack of Mars",
+        content: "The Speckled Statue"
+    },
+    {
+        title: "The Dance of the Raven",
+        description: "Devil's Voyage",
+        content: "Throne and the Shield"
+    },
+    {
+        title: "The Crooked Sun",
+        description: "The Owl in the Mist",
+        content: "Hiddne Midnight"
+    }
+];
 </script>
 
 <style scoped>
