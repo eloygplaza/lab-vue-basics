99c60394eb921a5548f47fa66f35c524766b5973
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-11 15:29:01 +0200

    iteration1 done

---
+++ lab-vue-basics/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar></Navbar>
+  
+  <Footer></Footer>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<style scoped>
+</style>
