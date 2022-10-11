b1c66ac4c296773f9e02afc41d9d80bb87e9fdb9
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-11 15:45:56 +0200

    iteration 2 done

--- lab-vue-basics/src/App.vue
+++ lab-vue-basics/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <Navbar></Navbar>
-  
+  <Content></Content>
   <Footer></Footer>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import Content from './components/Content.vue';
 </script>
 
 <style scoped>
--- lab-vue-basics/src/components/Content.vue
+++ lab-vue-basics/src/components/Content.vue
@@ -1,9 +1,18 @@
 <template>
+<h1 class="is-size-1">Iteration 1</h1>
+<h2>Componentes Navbar y Footer añadidos</h2>
 
-
+<h1 class="is-size-1">Iteration 2</h1>
+<h2>Mostar suma: "{ { 2 + 2 } }" = {{ 2 + 2 }}</h2>
+<h2>Mostrar texto de una funcion: {{showString()}}</h2>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+function showString (){
+    return ref("Hola");
+}
 </script>
 
 <style scoped>
